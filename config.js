9038a1ae1e0aacb23d796de37053bf2d7fc72977
Dark <darkbeamer.official@gmail.com>
2021-11-17 16:31:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['60197588090', '6281334177340'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -27,8 +27,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_name = 'Sticker by'
+  var sticker_author = 'Darks Bot'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -46,7 +46,7 @@ global.packname = sticker_name
 global.author = sticker_author
 
 
-global.multiplier = 69 // The higher, The harder levelup
+global.multiplier = 55 // The higher, The harder levelup
 
 
 let file = require.resolve(__filename)
